6e42d6fd5412165c10945c35b5e72fe6b73eb8da
Raúl Higueras <raulhigueras00@gmail.com>
2021-09-18 15:04:18 +0200

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='src',
     packages=find_packages(),
     version='0.1.0',
-    description='Natural Language Processing analysis of developers' written messages in the Technical Debt Database.',
+    description="Natural Language Processing analysis of developers' written messages in the Technical Debt Database.",
     author='Tim Team',
     license='MIT',
 )
